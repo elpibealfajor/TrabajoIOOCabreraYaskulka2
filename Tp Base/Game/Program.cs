2e989e0d39e2265d1de12e636bbd83274b1f8513
Cabrera Juan Manuel <63826558+elpibealfajor@users.noreply.github.com>
2020-09-28 12:34:59 -0300

    Add files via upload

---
+++ Tp Base/Game/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Game
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Engine.Initialize();
+
+            while(true)
+            {
+                Engine.Clear();
+                Engine.Draw("ship.png");
+                Engine.Show();
+            }
+        }
+    }
+}
